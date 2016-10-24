bfc9f9c317bbb2a79e31f977b5b8bc787a85bc31
Ian Hom <ianhom@163.com>
2016-10-24 08:45:13 +0800

    Create PythonNoob.sh

---
+++ Note_2/PythonNoob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is used to get Python-Noob from GitHub.
+
+cd 
+cd Desktop
+mkdir pytest
+cd pytest
+git clone https://github.com/ianhom/Python-Noob.git
+cd Python_Noob/Note_2
+
+chmod +x box.py
+
+./box.py
