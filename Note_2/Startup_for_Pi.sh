059cf71031302b3b5a435d9f336dc3e0ce012279
Ian Hom <ianhom@163.com>
2016-11-04 08:39:28 +0800

    Update Startup_for_Pi.sh

--- Note_2/Startup_for_Pi.sh
+++ Note_2/Startup_for_Pi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This script is used to git down my MOE and Python project from GitHub.
-cd 
-cd Desktop/Shell-Noob/Note_2
+
+u=$(whoami)
+cd /home/$u/Desktop/Shell-Noob/Note_2
 chmod +x ./*.sh
 ./MOE.sh
 ./PythonNoob.sh
