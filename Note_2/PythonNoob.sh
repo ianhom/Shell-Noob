aab5a9167f39d47dccc0c15eb432302806b6da8d
Ian Hom <ianhom@163.com>
2016-11-28 21:35:22 +0800

    Update PythonNoob.sh

--- Note_2/PythonNoob.sh
+++ Note_2/PythonNoob.sh
@@ -6,7 +6,7 @@ u=$(whoami)
 cd /home/$u/Desktop
 mkdir pytest
 cd pytest
-git clone https://github.com/ianhom/Python-Noob.git
+git clone https://github.com/ianhom/Python-Noob.git   # For some situation, use git://github.com/ianhom/Python-Noob.git
 cd Python-Noob/Note_2
 chmod +x *.py
 
